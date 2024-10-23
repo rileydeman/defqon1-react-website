e031a34d686b7a9221c8b2c2d83a8da3c7b072b0
Riley de Man <rileydeman@gmail.com>
2024-10-23 15:41:59 +0200

    Added empty header
    Added empty header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import CounterRoot from "./components/counter-provider.tsx";
 import Counter from "./components/counter.tsx";
+import Header from "./components/header.tsx";
 
 function App() {
   return (
       <>
+          <Header/>
           <CounterRoot>
               <div>
                   <h1>This is my CounterApp</h1>
