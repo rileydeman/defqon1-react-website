6fb09383326e6c782313601c53f088aee3fbaff0
Riley de Man <rileydeman@gmail.com>
2024-11-04 20:29:02 +0100

    Error in theme switch solved
    Error in theme switch solved

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -4,16 +4,20 @@ import {Switch} from "@mui/joy";
 export default function SwitchBtn() {
     const [checked, setChecked] = useState<boolean>(false);
 
-    const switchTheme = ():void => {
-        if (checked) {
-            document.querySelector("body").setAttribute("data-theme", "dark");
-            document.getElementById("headIcon").setAttribute("src", "/src/assets/img/defqon-icon-white.png");
-        } else {
-            document.querySelector("body").setAttribute("data-theme", "light");
-            document.getElementById("headIcon").setAttribute("src", "/src/assets/img/defqon-icon-black.png");
-        }
+    const switchTheme = (): void => {
+        const body = document.querySelector("body");
+        const headIcon = document.getElementById("headIcon");
 
-        setChecked(!checked);
+        if (body && headIcon) {
+            if (checked) {
+                body.setAttribute("data-theme", "dark");
+                headIcon.setAttribute("src", "/src/assets/img/defqon-icon-white.png");
+            } else {
+                body.setAttribute("data-theme", "light");
+                headIcon.setAttribute("src", "/src/assets/img/defqon-icon-black.png");
+            }
+            setChecked(!checked);
+        }
     };
 
     return(
