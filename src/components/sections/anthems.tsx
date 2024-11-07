221521337e211e5f6b44d9bc46ff62d1f1f96104
Riley de Man <rileydeman@gmail.com>
2024-11-08 00:49:22 +0100

    Anthems ready
    Anthems ready

--- src/components/sections/anthems.tsx
+++ src/components/sections/anthems.tsx
@@ -1,10 +1,32 @@
+import anthems from "../../assets/lists/anthems.json";
+import {Button} from "@mui/joy";
+
 export default function Anthems() {
     return(
         <>
             <section id="anthems">
                 <h2>Anthems</h2>
-                div
-                <img src="https://i.scdn.co/image/ab67616d00001e0298253ddd95edcb351a98804b" alt=""/>
+                <div id="anthems-repeater">
+                    {anthems.map((anthem) => (
+                        <div className="anthem-item">
+                            <figure>
+                                <img src={anthem.cover} alt=""/>
+                            </figure>
+                            <figcaption>
+                                <p>{anthem.year}</p>
+                                <p className="anthem-title">{anthem.name.toUpperCase()}</p>
+                                <p>{anthem.dj}</p>
+                                <Button
+                                    color="danger"
+                                    onClick={function(){window.open(anthem.spotify,'_blank')}}
+                                    size="sm" >
+                                    Listen on Spotify
+                                </Button>
+                            </figcaption>
+                        </div>
+                    ))}
+
+                </div>
             </section>
         </>
     );
