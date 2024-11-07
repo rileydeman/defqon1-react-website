a50b028be851d96a3a3685ef35bf7c635e0b5052
Riley de Man <rileydeman@gmail.com>
2024-11-07 23:52:52 +0100

    Start at the anthems
    Start at the anthems

--- src/components/sections/anthems.tsx
+++ src/components/sections/anthems.tsx
@@ -2,7 +2,9 @@ export default function Anthems() {
     return(
         <>
             <section id="anthems">
-                <p>Anthems</p>
+                <h2>Anthems</h2>
+                div
+                <img src="https://i.scdn.co/image/ab67616d00001e0298253ddd95edcb351a98804b" alt=""/>
             </section>
         </>
     );
